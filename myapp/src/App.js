0af53df5e9dfac5c7d056da9390934ebe4aa92c8
michaelbobkov <michaelbobkov1@gmail.com>
2024-01-08 12:37:25 +0100

    Added push on the page

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,20 +1,27 @@
 import NewComponent from "./NewComponent";
 import AddPerson from "./AddPerson"
+import React, { useState } from "react";
 
 function App() {
 
-  var people = [
-    {name: "Ben", age: 20, Tshirt: "black", id: 0},
-    {name: "Bam", age: 22, Tshirt: "green", id: 1},
-    {name: "Sam", age: 220, Tshirt: "yellow", id: 2}
-  ]
+  const [people, setPeople] = useState([
+    { name: "Ben", age: 20, Tshirt: "black", id: 0 },
+    { name: "Bam", age: 22, Tshirt: "green", id: 1 },
+    { name: "Sam", age: 220, Tshirt: "yellow", id: 2 }
+  ])
+
+  const addPerson = (event, person) => {
+    event.preventDefault()
+    person.id = Math.random()
+    setPeople([...people, person])
+  };
 
   return (
     <div className="App">
       <h1>My first React app!</h1>
       <p>Hello!</p>
       <NewComponent people={people}/>
-      <AddPerson />
+      <AddPerson addPerson={addPerson}/>
     </div>
   );
 }
